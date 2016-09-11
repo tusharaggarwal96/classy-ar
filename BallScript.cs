bc58492953772591750858d17808b1f429a09594
tusharaggarwal96 <tusharaggarwal96@yahoo.com>
2016-09-11 13:36:19 +0530

    Add files via upload

---
+++ BallScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+    public GameObject plane;
+
+    public GameObject spawnpoint;
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+	 if(transform.position.y < plane.transform.position.y -20)
+        {
+            transform.position = spawnpoint.transform.position;
+
+        }
+	}
+}
